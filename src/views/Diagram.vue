5fccffdca67efa4d1c30fdaed6fc390284784c9d
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2019-08-11 01:41:09 +0200

    Update Diagram.vue

--- src/views/Diagram.vue
+++ src/views/Diagram.vue
@@ -22,8 +22,8 @@ export default {
     createSvg(){
       // 2. Use the margin convention practice 
       var margin = {top: 50, right: 50, bottom: 50, left: 50}
-        , width = 900 - margin.left - margin.right
-        , height = 600 - margin.top - margin.bottom;
+        , width = window.innerWidth - margin.left - margin.right
+        , height = window.innerHeight - margin.top - margin.bottom;
 
       // The number of datapoints
       var n = 21;
