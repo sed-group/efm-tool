486a73ba66b8f9ceec677cfba72c9c07854cff75
Iñigo Alonso Fernandez <inigoa@net.chalmers.se>
2019-08-15 19:51:35 +0200

    took away the desserts

--- src/components/project/ViewProject.vue
+++ src/components/project/ViewProject.vue
@@ -70,7 +70,7 @@
                 ></v-text-field>
                 <v-data-table
                   :headers="headers"
-                  :items="desserts"
+                  :items="nodes"
                   :items-per-page="10"
                   single-select
                   item-key="name"
@@ -119,108 +119,17 @@ export default {
       snackbarDeletedProject: false,
       search: '',
       headers: [
-        {
-          text: 'Dessert (100g serving)',
-          align: 'left',
-          sortable: false,
-          value: 'name',
-        },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' },
+        { text: 'Node name', value: 'name', align: 'left' },
+        { text: 'Description', value: 'description' },
+        { text: 'Parent', value: 'parent' },
+        { text: 'Type', value: 'type' },
       ],
-      desserts: [
+      nodes: [
         {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%',
-          type: 'delicious',
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%',
-          type: 'delicious',
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%',
-          type: 'delicious',
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%',
-          type: 'delicious',
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%',
-          type: 'delicious',
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%',
-          type: 'delicious',
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%',
-          type: 'meh',
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%',
-          type: 'meh',
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%',
-          type: 'meh',
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%',
-          type: 'meh',
+          name: 'A good node',
+          description: 'A nice description of this node',
+          parent: 'The parent node',
+          type: 'DS',
         },
       ],
     }
